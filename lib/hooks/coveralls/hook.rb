dab42cebcad93a4b7a6465599690bd0815759e5a
asonas <hzw1258@gmail.com>
2014-08-11 14:07:10 +0900

    Add covaralls

---
+++ lib/hooks/coveralls/hook.rb
@@ -0,0 +1,7 @@
+module Idobata::Hook
+  class Coveralls < Base
+    screen_name 'Coveralls'
+    identifier  :coveralls
+    icon_url    "https://coveralls.io/apple-touch-icon-precomposed.png"
+  end
+end
